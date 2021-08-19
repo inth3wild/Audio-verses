788e3ddf170b2cd1c865757cecf97b748eaaa377
inth3wild <toochukeuchukwu@gmail.com>
2021-08-19 13:27:39 +0100

    started work

---
+++ app/config/config.py
@@ -0,0 +1,31 @@
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+class Base(BaseSettings):
+    class Config:
+        env_file = str( load_dotenv( Path.cwd()/".env" ) )
+
+class CommonSettings(Base):
+    APP_NAME: str = "Audio verses"
+    DEBUG_MODE: bool
+
+class ServerSettings(Base):
+    HOST: str = "127.0.0.1"
+    PORT: int = 9000
+
+class DatabaseSettings(Base):
+    DB_URL: str
+    DB_NAME: str
+
+class TokenSettings(Base):
+    JWT_SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+
+class Settings(CommonSettings, ServerSettings, DatabaseSettings, TokenSettings):
+    pass
+
+settings = Settings()
---
+++ app/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.applications import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from motor.motor_asyncio import AsyncIOMotorClient
+from config.config import settings
+
+# Import routes
+
+
+
+# Init app
+app = FastAPI()
+
+# Init DB
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
+    app.mongodb = app.mongodb_client[settings.DB_NAME]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
+
+
+
+# Include routers
+
+
+@app.get("/")
+@app.get("/home")
+async def home_route():
+    return {"message": "Welcome to the home route"}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        reload=settings.DEBUG_MODE,
+        port=settings.PORT,
+    )
