728182ba4e3d271c636045e63fb43350c02b2a29
inth3wild <toochukeuchukwu@gmail.com>
2021-08-20 21:59:17 +0100

    started working on the processing file function

---
+++ app/models/audio.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+
+
+class AudioIn(BaseModel):
+    audio_id: int
--- app/routers/apiRoutes.py
+++ app/routers/apiRoutes.py
@@ -1,15 +1,22 @@
 from fastapi import APIRouter, HTTPException, Request, Response, File, UploadFile
 # from models.token import create_access_token, TokenData
+from models.audio import AudioIn
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import HTMLResponse
 from config.config import settings
 from fastapi.templating import Jinja2Templates
 
+import speech_recognition as sr
+from pathlib import Path
+
+
+r = sr.Recognizer()
 
 
 router = APIRouter(
 )
 templates = Jinja2Templates(directory="templates")
+
 
 @router.get("/", response_class=HTMLResponse, status_code=200)
 async def home(request: Request):
@@ -22,7 +29,14 @@ async def listen():
     return {"msg": "started listening"}
 
 
-
 @router.post("/process", status_code=200)
-async def process_file(file: UploadFile = File(...) ):
-    return {"filename": file.filename}
+async def process_file(audio_in: AudioIn):
+    audio_file = Path.cwd()/'audio-files'/f"{audio_in.audio_id}.wav" 
+
+    my = sr.AudioFile( str(audio_file) )
+    with my as source:
+        audio = r.record(source)
+
+    print ( r.recognize_google(audio) )
+    # print(audio_in.audio_id)
+    return {"msg":"good"}
