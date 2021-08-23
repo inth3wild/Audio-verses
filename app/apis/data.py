3967c30a4325262925d27ac082a9fe641caddeae
inth3wild <toochukeuchukwu@gmail.com>
2021-08-23 12:23:47 +0100

    started working on bible api calls

---
+++ app/apis/data.py
@@ -0,0 +1,9 @@
+bible_ids = {
+    "King James (Authorised) Version": "de4e12af7f28f599-02",
+
+    "Revised Version 1885": "40072c4a5aba4022-01",
+
+    "Biblica® Open Hausa Contemporary Bible 2020": "0ab0c764d56a715d-02",
+
+    "Biblica® Open Yoruba Contemporary Bible 2017": "b8d1feac6e94bd74-01"
+}
---
+++ app/apis/requests.py
@@ -0,0 +1,17 @@
+import httpx
+
+# from config.config import settings
+from models.audio import AudioIn
+# import config
+
+base_url = "https://api.scripture.api.bible/v1/bibles/"
+# api_key = settings.API_BIBLE_API_KEY
+
+# print(api_key)
+print(AudioIn)
+
+
+
+def search():
+    return
+    ...
