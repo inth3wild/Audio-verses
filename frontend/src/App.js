58b74eee391de5daffadae4ba1b3b8bbddff8a66
inth3wild <toochukeuchukwu@gmail.com>
2021-10-01 18:52:15 +0100

    started react aspect

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        lol
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return ( 
+        <div className="container">
+            <nav className="navbar navbar-light sticky-top">
+                <div className="container-fluid">
+                    <Link to='/'> <i className="bi bi-soundwave"></i> Audio Verses </Link>
+                </div>
+
+                <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                    <div className="btn-group btn-group-lg me-2" role="group" aria-label="First group">
+                        <button type="button" className="btn active" data-bs-toggle="button" id="light"><i className="bi bi-brightness-high"></i></button>
+                        <button type="button" className="btn" id="dark"><i className="bi bi-moon-fill"></i></button>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+ 
+export default Navbar;
